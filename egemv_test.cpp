c76fef3623de54dc228f6a8735651b2455a93614
Ethan Steere <62357808+e253@users.noreply.github.com>
2024-04-19 18:08:02 -0400

    one more egemv test. still needs fuzzing and more complicated cases to be sure.

--- egemv_test.cpp
+++ egemv_test.cpp
@@ -9,8 +9,9 @@ void egemv_test1()
     int n = 14336;
 
     uint8_t* w = (uint8_t*)_alloc(m * n / 2, 64);
-    for (int i = 0; i < m * n / 2; i++)
+    for (int i = 0; i < m * n / 2; i++) {
         w[i] = 0x55;
+    }
 
     float* s = (float*)_alloc(m * n / QBLOCK_SIZE * sizeof(float), 64);
     for (int i = 0; i < m * n / QBLOCK_SIZE; i++)
@@ -96,10 +97,64 @@ void egemv_test2()
     }
 }
 
+void egemv_test3()
+{
+    int m = 4096;
+    int n = 14336;
+
+    uint8_t* w = (uint8_t*)_alloc(m * n / 2, 64);
+    for (int i = 0; i < m * n / 2; i++)
+        w[i] = 0x55;
+
+    float* s = (float*)_alloc(m * n / QBLOCK_SIZE * sizeof(float), 64);
+    for (int j = 0; j < m; j++) { // row idx
+        for (int i = 0; i < n / QBLOCK_SIZE; i++) { // col idx
+            if (i % 2 == 0) {
+                s[i * m + j] = 1.0f;
+            } else {
+                s[i * m + j] = 2.0f;
+            }
+        }
+    }
+
+    uint8_t* z = (uint8_t*)_alloc(m * n / QBLOCK_SIZE / 2, 64);
+    for (int i = 0; i < m * n / QBLOCK_SIZE / 2; i++)
+        z[i] = 0x11;
+
+    float* in = (float*)_alloc(n * sizeof(float), 64);
+    for (int i = 0; i < n; i++)
+        in[i] = 2.0f;
+
+    float* out = (float*)_alloc(m * sizeof(float), 64);
+    memset(out, 0, m * sizeof(float));
+
+    q4f32s_egemv(
+        w, s, z,
+        in, out,
+        m, n);
+
+    bool passed = true;
+    for (int i = 0; i < m; i++) {
+        if (out[i] != 114688.0f) {
+            if (i < 50) {
+                std::cout << "Output[" << i << "] = " << out[i] << std::endl;
+            }
+            passed = false;
+        }
+    }
+    if (!passed) {
+        std::cout << "Egemv Test 3 Failed" << std::endl;
+        exit(0);
+    } else {
+        std::cout << "Egemv Test 3 Passed" << std::endl;
+    }
+}
+
 int main()
 {
     std::cout << "Egemv V" << VER << " Test" << std::endl;
     egemv_test1();
     egemv_test2();
+    egemv_test3();
     std::cout << std::endl;
 }

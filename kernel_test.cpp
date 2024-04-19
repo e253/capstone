bb38e7ea0bff333eb99e5a06b49536b9e64e8b30
Ethan Steere <62357808+e253@users.noreply.github.com>
2024-04-19 19:51:45 -0400

    added another kernel test

--- kernel_test.cpp
+++ kernel_test.cpp
@@ -191,6 +191,80 @@ void kernel_test3()
     _free(Output);
 }
 
+void kernel_test4()
+{
+    int m = 128;
+    int n = 512;
+
+    uint8_t* Weights = (uint8_t*)_alloc(m * n / 2, 64);
+    for (int j = 0; j < m / 2; j++) { // row idx
+        for (int i = 0; i < n; i++) { // col idx
+            if (j % 2 == 0) {
+                Weights[i * m / 2 + j] = 0x33;
+            } else {
+                Weights[i * m / 2 + j] = 0x55;
+            }
+        }
+    }
+
+    float* W_Scales = (float*)_alloc(m * n / QBLOCK_SIZE * sizeof(float), 64);
+    for (int j = 0; j < m; j++) { // row idx
+        for (int i = 0; i < n / QBLOCK_SIZE; i++) { // col idx
+            W_Scales[i * m + j] = 2.0f;
+        }
+    }
+
+    uint8_t* W_Zeros = (uint8_t*)_alloc(m * n / QBLOCK_SIZE / 2, 64);
+    for (int i = 0; i < m * n / QBLOCK_SIZE / 2; i++)
+        W_Zeros[i] = 0x11;
+
+    float* Input = (float*)_alloc(n * sizeof(float), 64);
+    for (int i = 0; i < n; i++)
+        Input[i] = 2.0f;
+
+    float* Output = (float*)_alloc(m * sizeof(float), 64);
+    std::memset(Output, 0, m * sizeof(float));
+
+    q4f32s_ukernel_prelude();
+    q4f32s_ukernel(
+        Weights, m / 2,
+        W_Scales, m,
+        W_Zeros, m / 2,
+        Input,
+        nullptr,
+        512);
+    q4f32s_ukernel_epiloque(Output);
+
+    bool passed = true;
+    for (int i = 0; i < m; i++) {
+        if (i % 2 == 0) {
+            if (Output[i] != 4096.0f) {
+                std::cout << "Output[" << i << "] = " << Output[i] << std::endl;
+                passed = false;
+            }
+        } else {
+            if (Output[i] != 8192.0f) {
+                std::cout << "Output[" << i << "] = " << Output[i] << std::endl;
+                passed = false;
+            }
+        }
+        
+    }
+    if (!passed) {
+        std::cout << std::endl;
+        std::cout << "Kernel Test 3 Failed" << std::endl;
+        exit(0);
+    } else {
+        std::cout << "Kernel Test 3 Passed" << std::endl;
+    }
+
+    _free(Weights);
+    _free(W_Scales);
+    _free(W_Zeros);
+    _free(Input);
+    _free(Output);
+}
+
 int main()
 {
     std::cout << "Kenel V" << KERNEL_VER << " Test" << std::endl;

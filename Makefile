629654e35a89a66261b324c5af56c54d17f60625
Ethan Steere <62357808+e253@users.noreply.github.com>
2024-04-26 17:07:55 -0400

    igpu program passing trivial vector add.

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 .PHONY: all fmt clean
 
-all:
+all: cpu igpu
+
+cpu: cpu.cpp
 	clang++ -march=native -O3 cpu.cpp -o cpu.exe
+
+igpu: igpu.cpp
+	clang++ -march=native -O3 igpu.cpp -o igpu.exe -lOpenCL
 
 fmt:
 	clang-format -style=WebKit -i *.cpp *.hpp
---
+++ igpu.cpp
@@ -0,0 +1,138 @@
+#include <CL/cl.h>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+#define CL_CHECK(status, op)                                            \
+    if (status != CL_SUCCESS) {                                         \
+        cout << "OpenCL error: " << status << " During " << op << endl; \
+        exit(1);                                                        \
+    }
+#define SVMMAP(queue, svm_ptr, size) clEnqueueSVMMap(queue, CL_TRUE, CL_MEM_READ_WRITE, svm_ptr, size, 0, NULL, NULL)
+#define SVMUNMAP(queue, svm_ptr) clEnqueueSVMUnmap(queue, svm_ptr, 0, NULL, NULL)
+#define BROADCAST(ptr, len, v)    \
+    for (int i = 0; i < len; i++) \
+    ptr[i] = v
+
+#define CL_SRC(...) #__VA_ARGS__
+const string cl_src = CL_SRC(
+
+    __kernel void vec_add(__global const float* a, __global const float* b, __global float* c, const int n) {
+        int i = get_global_id(0);
+        if (i < n) {
+            c[i] = a[i] + b[i];
+        }
+    }
+
+);
+
+static cl_context context;
+static cl_kernel vec_add_kernel;
+static cl_command_queue queue;
+
+void test_vec_add()
+{
+    const int n = 1024;
+
+    float* a = (float*)clSVMAlloc(context, CL_MEM_READ_WRITE, n * sizeof(float), 64);
+    float* b = (float*)clSVMAlloc(context, CL_MEM_READ_WRITE, n * sizeof(float), 64);
+    float* c = (float*)clSVMAlloc(context, CL_MEM_READ_WRITE, n * sizeof(float), 64);
+
+    SVMMAP(queue, a, n * sizeof(float));
+    BROADCAST(a, n, 1.0f);
+    SVMUNMAP(queue, a);
+
+    SVMMAP(queue, b, n * sizeof(float));
+    BROADCAST(b, n, 2.0f);
+    SVMUNMAP(queue, b);
+
+    SVMMAP(queue, c, n * sizeof(float));
+    BROADCAST(c, n, 0.0f);
+    SVMUNMAP(queue, c);
+
+    cl_int clStatus;
+
+    clStatus = clSetKernelArgSVMPointer(vec_add_kernel, 0, a);
+    CL_CHECK(clStatus, "clSetKernelArgSVMPointer - a")
+    clStatus = clSetKernelArgSVMPointer(vec_add_kernel, 1, b);
+    CL_CHECK(clStatus, "clSetKernelArgSVMPointer - b")
+    clStatus = clSetKernelArgSVMPointer(vec_add_kernel, 2, c);
+    CL_CHECK(clStatus, "clSetKernelArgSVMPointer - c")
+    clSetKernelArg(vec_add_kernel, 3, sizeof(int), &n);
+
+    cl_event event;
+    const size_t global_work_size = n;
+    const size_t local_work_size = 1;
+    clStatus = clEnqueueNDRangeKernel(
+        queue, vec_add_kernel,
+        1, nullptr,
+        &global_work_size, &local_work_size,
+        0, nullptr, &event);
+    CL_CHECK(clStatus, "vec add kernel");
+
+    clWaitForEvents(1, &event);
+
+    SVMMAP(queue, c, n * sizeof(float));
+    bool passed = true;
+    for (int i = 0; i < n; i++) {
+        if (c[i] != 3.0f) {
+            cout << "Error: c[" << i << "] = " << c[i] << endl;
+            passed = false;
+        }
+    }
+    if (passed) {
+        cout << "Vector Add Passed!" << endl;
+    } else {
+        cout << "Vector Add Failed!" << endl;
+    }
+
+    clSVMFree(context, a);
+    clSVMFree(context, b);
+    clSVMFree(context, c);
+}
+
+int main()
+{
+    cl_int clStatus;
+    cl_device_id device[1];
+    cl_uint numDevices = 1;
+    clStatus = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, numDevices, device, NULL);
+    CL_CHECK(clStatus, "clGetDeviceIDs");
+
+    context = clCreateContext(NULL, 1, device, NULL, NULL, &clStatus);
+    CL_CHECK(clStatus, "clCreateContext");
+
+    // cl_queue_properties queue_props[] = { CL_QUEUE_PROPERTIES, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, CL_QUEUE_ON_DEVICE };
+    queue = clCreateCommandQueueWithProperties(context, device[0], nullptr, &clStatus);
+    CL_CHECK(clStatus, "clCreateCommandQueue");
+
+    // build kernels
+    char* c_str_cl_src = (char*)cl_src.c_str();
+    cl_program p = clCreateProgramWithSource(context, 1, (const char**)&c_str_cl_src, NULL, &clStatus);
+    CL_CHECK(clStatus, "clCreateProgramWithSource");
+    string CLC_FLAGS = "-cl-std=CL2.0 -cl-mad-enable -cl-fast-relaxed-math";
+    clStatus = clBuildProgram(p, 1, device, CLC_FLAGS.c_str(), NULL, NULL);
+    if (clStatus != CL_SUCCESS) {
+        size_t log_size;
+        clGetProgramBuildInfo(p, device[0], CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+        char* log = (char*)malloc(log_size + 1);
+        clGetProgramBuildInfo(p, device[0], CL_PROGRAM_BUILD_LOG, log_size + 1, log, NULL);
+        cout << "Build Errors:\n\n"
+             << log << endl;
+        free(log);
+        exit(1);
+    }
+
+    vec_add_kernel = clCreateKernel(p, "vec_add", &clStatus);
+    CL_CHECK(clStatus, "clCreateKernel - vec_add");
+
+    // test kernels
+    test_vec_add();
+
+    // cleanup
+    clReleaseKernel(vec_add_kernel);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
+}
